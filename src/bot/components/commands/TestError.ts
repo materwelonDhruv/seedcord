05e94c5e9e171d2881a348c0eac7c96ef37bb0d0
materwelon <materwelondhruv@gmail.com>
2025-07-15 02:32:19 +0530

    fix: missing command annotation

--- src/bot/components/commands/TestError.ts
+++ src/bot/components/commands/TestError.ts
@@ -1,6 +1,8 @@
 import { PermissionFlagsBits } from 'discord.js';
 import { BuilderComponent } from '../../interfaces/Components';
+import { RegisterCommand } from '../../decorators/CommandRegisterable';
 
+@RegisterCommand('global')
 export class TestErrorCommand extends BuilderComponent<'command'> {
   constructor() {
     super('command');
