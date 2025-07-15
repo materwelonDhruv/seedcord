0d36f06c7a4f0f86e8688860429fbb41d0236b2c
materwelon <materwelondhruv@gmail.com>
2025-07-15 12:58:29 +0530

    fix: remove constraint causing any string

--- src/core/database/types/Services.ts
+++ src/core/database/types/Services.ts
@@ -1,11 +1,6 @@
-import { BaseService } from '../BaseService';
-
-// Base constraint for any service map - all values must extend BaseService
-export type ServiceMapConstraint = Record<string, BaseService<any>>;
-
 // This interface can be augmented via declaration merging
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Services extends ServiceMapConstraint {}
+export interface Services {}
 
 // Helper type to extract service keys
 export type ServiceMapKeys = keyof Services;
