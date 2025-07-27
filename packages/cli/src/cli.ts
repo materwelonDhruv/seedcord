0ecc8e1e380b866f7434e0778109833a3d878c28
materwelon <materwelondhruv@gmail.com>
2025-07-28 01:34:41 +0530

    chore: turbo repo

---
+++ packages/cli/src/cli.ts
@@ -0,0 +1,12 @@
+import chalk from 'chalk';
+
+// eslint-disable-next-line no-console
+console.log(chalk.blue('🌱 Seedcord CLI'));
+// eslint-disable-next-line no-console
+console.log(chalk.gray('Version 0.1.0'));
+
+// CLI implementation placeholder
+export function main(): void {
+  // eslint-disable-next-line no-console
+  console.log('CLI functionality coming soon!');
+}
