45a6242c2d9ffc9c0b48bad5c07cd38ff3aa26ec
materwelon <materwelondhruv@gmail.com>
2025-07-30 14:18:09 +0530

    chore: move envapt to separate repository

--- packages/envapt/eslint.config.mjs
+++
@@ -1,5 +0,0 @@
-import createConfig from '@seedcord/eslint-config/create-config';
-
-export default createConfig({
-  tsconfigRootDir: import.meta.dirname
-});
--- packages/envapt/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { Envapt } from './Envapt';
-export { Envapter, Environment } from './Envapter';
-export { EnvaptError, EnvaptErrorCodes } from './Error';
-
-export type { EnvaptErrorCode } from './Error';
-export type * from './Types';
