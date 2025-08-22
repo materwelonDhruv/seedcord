a0363dbaeb1ca844e192585649ced2d127d1c327
materwelon <materwelondhruv@gmail.com>
2025-08-22 10:08:06 -0700

    fix(WIP): eslint-config build issues

---
+++ packages/eslint-config/tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  format: ['esm', 'cjs'],
+  entry: ['src/index.ts'],
+  dts: true,
+  shims: true,
+  skipNodeModulesBundle: true,
+  clean: true,
+  treeshake: true,
+  platform: 'node',
+  target: 'es2022',
+  splitting: false,
+  cjsInterop: true,
+  minify: false,
+  keepNames: true,
+  sourcemap: true,
+  outDir: 'dist',
+  outExtension: (ctx) => {
+    if (ctx.format === 'cjs') return { js: '.cjs' };
+    if (ctx.format === 'esm') return { js: '.mjs' };
+    return { js: '.js' };
+  }
+});
