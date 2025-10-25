e2b571f225b1e4d2532be76b6f9a3d030ae7f113
materwelonDhruv <materwelondhruv@gmail.com>
2025-10-24 22:16:56 -0700

    chore: use mjs prettier config instead of ...rc

---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import("prettier").Config} */
+const config = {
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    printWidth: 120,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    useTabs: false,
+    quoteProps: 'as-needed',
+    bracketSameLine: false,
+    proseWrap: 'preserve'
+};
+
+export default config;
