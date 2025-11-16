eb7de1fea2a50e7a18255e53cee0819625bf4357
materwelonDhruv <materwelondhruv@gmail.com>
2025-11-16 02:20:52 -0700

    fix: configurable defer check was flawed

--- packages/seedcord/src/bot/decorators/Confirmable/Confirmable.ts
+++ packages/seedcord/src/bot/decorators/Confirmable/Confirmable.ts
@@ -97,7 +97,9 @@ const shouldDefer = <TComponentType extends MessageComponentType>(
 ): boolean => {
     if (ix.deferred) return false;
     if (opts.defer === false) return false;
-    return isSlash || isContext;
+    if (isSlash || isContext) return true;
+    if (isMessageComponentIx(ix)) return true;
+    return false;
 };
 
 const maybeDefer = async <TComponentType extends MessageComponentType>(
